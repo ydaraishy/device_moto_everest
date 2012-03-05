018f57613c5426ed546dfc4dc21c243f4a10afc2
ydaraishy <ydaraishy@ydaraishy.org>
2012-03-05 19:38:02 +1100

    Add script to patch files for everest.

---
+++ patch/patch.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+patch ../../../../frameworks/base/telephony/java/com/android/internal/telephony/gsm/GsmDataConnectionTracker.java  GsmDataConnectionTracker.p
+
+patch ../../../../frameworks/base/telephony/java/com/android/internal/telephony/gsm/GsmServiceStateTracker.java  GsmServiceStateTracker.p 
