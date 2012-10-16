bb200ae499cb75e6dfaaa89e858279ee41691485
ydaraishy <ydaraishy@ydaraishy.org>
2012-10-16 22:37:38 +1100

    Add in new drivers.

--- get_extractors.sh
+++ get_extractors.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-BROADCOM_DRIVER=broadcom-wingray-imm76d-ba230f0a.tgz
-NVIDIA_DRIVER=nvidia-wingray-imm76d-f793a3bf.tgz
+SRC_ROOT=`dirname $0`/../../..
+
+BROADCOM_DRIVER=broadcom-wingray-jzo54k-78dd7c25.tgz
+NVIDIA_DRIVER=nvidia-wingray-jzo54k-3b2982d4.tgz
 
 wget https://dl.google.com/dl/android/aosp/$BROADCOM_DRIVER
 wget https://dl.google.com/dl/android/aosp/$NVIDIA_DRIVER
@@ -9,8 +11,8 @@ wget https://dl.google.com/dl/android/aosp/$NVIDIA_DRIVER
 tar xzf $BROADCOM_DRIVER
 tar xzf $NVIDIA_DRIVER
 
-mv extract-broadcom-wingray.sh ../../../
-mv extract-nvidia-wingray.sh ../../../
+mv extract-broadcom-wingray.sh $SRC_ROOT
+mv extract-nvidia-wingray.sh $SRC_ROOT
 
 rm $BROADCOM_DRIVER
 rm $NVIDIA_DRIVER
